f75d51479d239a7759b1e051c890db0dd13ea1f4
termanico <klingbeil.thorsten@t-online.de>
2021-02-02 17:59:44 +0000

    Update Ex4

---
+++ app/pofiori/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("sap.teched.pofiori.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
